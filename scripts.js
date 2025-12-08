f598cbcaab7107905bd53431228a062ecf9b0203
jessebabcock <jessebabcock98@gmail.com>
2025-12-08 08:02:33 -0600

    updated scripts.js and font-family in styles.css

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
+document.querySelectorAll(".image-set").forEach(function (element){
+    element.addEventListener('mouseenter', function(){
+        const infoBubble = element.querySelector(".info");
+        infoBubble.style.visibility = 'hidden';
+    });
 
-
-document.addEventListener('DOMContentLoaded', function (){
-    document.querySelectorAll(".image-set").forEach(function (element){
-        element.addEventListener('mouseenter', function(){
-            const infoBubble = element.querySelector(".info");
-            infoBubble.style.visibility = 'hidden';
-        });
-
-        element.addEventListener('mouseleave', function(){
-            element.querySelector(".info").style.visibility = 'visible';
-        });
+    element.addEventListener('mouseleave', function(){
+        element.querySelector(".info").style.visibility = 'visible';
     });
 });
+
