159f28bc1fe49aea24717810b2431fa5778b28b7
jessebabcock <jessebabcock98@gmail.com>
2025-12-09 00:01:57 -0600

    added handling for resizing window

--- scripts.js
+++ scripts.js
@@ -1,16 +1,4 @@
-document.querySelectorAll(".image-set").forEach(function (element){
-    element.addEventListener('mouseenter', function(){
-        const infoBubble = element.querySelector(".info");
-        infoBubble.style.visibility = 'hidden';
-    });
-
-    element.addEventListener('mouseleave', function(){
-        element.querySelector(".info").style.visibility = 'visible';
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-
+function catPositionCalc(){
     const catPic = document.getElementById("cat_container");
     //absolute regex banger
     // grabs filepath name '/personal_website/index.html' -> index
@@ -23,7 +11,23 @@ document.addEventListener("DOMContentLoaded", function() {
     //center cat on buttons
     catPic.style.left = (homeButton.offsetLeft + homeButton.offsetWidth / 2 - catPic.offsetWidth / 2).toString() + "px";
     catPic.style.top = (homeButton.offsetTop - 40).toString() + "px";
+}
 
+
+document.querySelectorAll(".image-set").forEach(function (element){
+    element.addEventListener('mouseenter', function(){
+        const infoBubble = element.querySelector(".info");
+        infoBubble.style.visibility = 'hidden';
+    });
+
+    element.addEventListener('mouseleave', function(){
+        element.querySelector(".info").style.visibility = 'visible';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('resize', catPositionCalc);
+    catPositionCalc();
 });
 
 
