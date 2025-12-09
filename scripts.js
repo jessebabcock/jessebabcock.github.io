df2ed64a978e0b973833309b66053bfc61b2ca24
jessebabcock <jessebabcock98@gmail.com>
2025-12-08 21:57:07 -0600

    insane regex pull

--- scripts.js
+++ scripts.js
@@ -9,3 +9,13 @@ document.querySelectorAll(".image-set").forEach(function (element){
     });
 });
 
+const catPic = document.getElementById("cat_container");
+//absolute regex banger
+// grabs filepath name '/personal_website/index.html' -> index
+const homeButton = document.getElementById(window.location.pathname.split(new RegExp('.*[\/]+([^.]*)'))[1].toString())
+
+//center cat on buttons
+catPic.style.left = (homeButton.offsetLeft + homeButton.offsetWidth / 2 - catPic.offsetWidth / 2).toString() + "px";
+catPic.style.top = (homeButton.offsetTop - 40).toString() + "px";
+
+
